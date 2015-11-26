2dd60f04b706033f8a97386a6bcb02510a5b596f
william <william@localhost.localdomain>
2015-11-26 17:49:43 +0800

    add PTO-Form

--- src/containers/PTOPage/PTOPage.js
+++ src/containers/PTOPage/PTOPage.js
@@ -3,12 +3,18 @@ import './_PTOPage';
 // React & Redux
 import React, { Component } from 'react';
 
+// Components
+import PTOForm from '../../components/PTO-Form/PTO-Form';
+
 class PTOPage extends Component {
-	render() {
-		return (
-			<section>PTO Page</section>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <section>PTO Page</section>
+                <PTOForm />
+            </div>
+        );
+    }
 }
 
 export default PTOPage;
