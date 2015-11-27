b7a2388724e1b82a9750bb2c6d4d9ad103f78a42
william <lflower1206@gmail.com>
2015-11-27 12:37:32 +0800

    add PTO-Table

--- src/containers/PTOPage/PTOPage.js
+++ src/containers/PTOPage/PTOPage.js
@@ -7,6 +7,7 @@ import ReactBsTable from 'react-bootstrap-table';
 
 // Components
 import PTOForm from '../../components/PTO-Form/PTO-Form';
+import PTOTable from '../../components/PTO-Table/PTO-Table';
 
 var BootstrapTable = ReactBsTable.BootstrapTable;
 var TableHeaderColumn = ReactBsTable.TableHeaderColumn;
@@ -22,12 +23,32 @@ var products = [
       price: 80
   }];
 
+let ptoData = [{
+    startDate: '2015-08-17',
+    toDate: '2015-08-18',
+    totalHours: 16,
+    applyDate: '2015-08-10',
+    isApproved: 'Approved',
+    memory: 'go home'
+},
+{
+    startDate: '2015-09-11',
+    toDate: '2015-09-13',
+    totalHours: 24,
+    applyDate: '2015-09-10',
+    isApproved: 'Not Approved',
+    memory: 'go home'
+}];
+
 class PTOPage extends Component {
     render() {
         return (
             <div>
                 <section>PTO Page</section>
-                <PTOForm />
+                <div className="mdl-grid">
+                    <PTOForm />
+                    <PTOTable data={ptoData} />
+                </div>
                 <BootstrapTable data={products} striped={true} hover={true} >
                     <TableHeaderColumn isKey={true} dataField="id" dataSort={true}>Product ID</TableHeaderColumn>
                     <TableHeaderColumn dataField="name" dataSort={true} >Product Name</TableHeaderColumn>
