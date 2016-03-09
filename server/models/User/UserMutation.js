2e1d80fe17ea48e3ed3cb1ee3efdaef6829e019c
william` <lflower1206@gmail.com>
2016-03-09 11:48:48 +0800

    login authorization with ldap

--- server/models/User/UserMutation.js
+++ server/models/User/UserMutation.js
@@ -10,162 +10,153 @@ import {
 import r from 'rethinkdb';
 // Constants
 import {
-	LDAP,
-	LDAP_AUTH_PREFIX,
-	SECURE_KEY,
-	DB_HOST,
-	DB_PORT
+    LDAP,
+    LDAP_AUTH_PREFIX,
+    SECURE_KEY,
+    DB_HOST,
+    DB_PORT
 } from '../../constants/configurations.js';
 
-function adPromise(account, password) {
+var loginPromise = function(username, password) {
+
+    var user = 'corp\\' + username;
+    var config = JSON.parse(JSON.stringify(LDAP));
+    var ad;
+
+    config['username'] = user;
+    config['password'] = password;
+
+    ad = new ActiveDirectory(config);
+
     return new Promise((resolve, reject) => {
- 		// try to login
-		let	ad = new ActiveDirectory(LDAP);
-		ad.authenticate(account, password, function(err, auth) {
-			if (err || !auth) {
-				console.log('Login Failed');
-                reject(new Error('Account or Password Incorrect!'));
-            } else {
-            	console.log('Login successfully');
-                resolve(auth);
-            }
-		});
+        authenticatePromise(ad, user, password)
+            .then(function(auth) {
+                return findUserPromise(ad, username);
+            }, function(err) {
+                console.log('login fail');
+                reject(err);
+            })
+            .then(function(user) {
+                resolve(user);
+            }, function(err) {
+                console.log('find user fail');
+                reject(err);
+            });
     });
+
+
+};
+
+var authenticatePromise = function(ad, user, password) {
+
+    return new Promise((resolve, reject) => {
+        ad.authenticate(user, password, function(err, auth) {
+
+            if (err) {
+                reject(new Error(JSON.stringify(err)));
+            }
+
+            if (auth) {
+                resolve(auth);
+            } else {
+                reject(new Error(JSON.stringify(err)));
+            }
+
+        });
+    });
+
+};
+
+var findUserPromise = function(ad, username) {
+
+    return new Promise((resolve, reject) => {
+        var query = {
+            filter: '(sAMAccountName=' + username + ')',
+            attributes: ['displayName', 'mail', 'title', 'sAMAccountName', 'uSNCreated', 'givenName']
+        };
+
+        ad.findUser(query, username, function(err, results) {
+            if (err) {
+                reject(new Error(JSON.stringify(err)));
+            }
+
+            if (results) {
+                resolve(results);
+            }
+        });
+    });
+
 };
 
 let UserMutation = {
-	'updateUserPrivilege': {
-		type: GraphQLString,
-		description: 'Set user privilege',
-		args: {
-			id: {
-				type: GraphQLString,
-				description: 'User\'s id'
-			},
-			privilege: {
-				type: GraphQLString,
-				description: 'User\'s privilege'
-			}
-		},
-		resolve: async (root, { id, privilege }) => {
-			console.log('updateUserPrivilege');
-			let connection = null,
-				mutationQuery = null;
-			try {
-				connection = await r.connect({ host: DB_HOST, port: DB_PORT });
-				mutationQuery = r.db('work_genius').table('users').get(id).update({privilege});
-				await mutationQuery.run(connection);
-				await connection.close();
-			} catch (err) {
-				return err;
-			}
+    'updateUserPrivilege': {
+        type: GraphQLString,
+        description: 'Set user privilege',
+        args: {
+            id: {
+                type: GraphQLString,
+                description: 'User\'s id'
+            },
+            privilege: {
+                type: GraphQLString,
+                description: 'User\'s privilege'
+            }
+        },
+        resolve: async (root, { id, privilege }) => {
+            let connection = null,
+                mutationQuery = null;
+            try {
+                connection = await r.connect({ host: DB_HOST, port: DB_PORT });
+                mutationQuery = r.db('work_genius').table('users').get(id).update({privilege});
+                await mutationQuery.run(connection);
+                await connection.close();
+            } catch (err) {
+                return err;
+            }
 
-			return 'Update successfully!';
-		}
-	},
-};
-
-const fakeExtract = function(account) {
-	switch (account) {
-		case 'howardc':
-			return {
-				id: '00001',
-				name: 'Howard Chang',
-				nickname: 'Howard C.',
-				email: 'howardc@a10networks.com'
-			};
-		case 'vlai':
-			return {
-				id: '00002',
-				name: 'Vans Lai',
-				nickname: 'Chia Wei L.',
-				email: 'vlai@a10networks.com'
-			};
-		case 'ahuang':
-			return {
-				id: '00003',
-				name: 'Albert Huang',
-				nickname: 'Albert H.',
-				email: 'ahuang@a10networks.com'
-			};
-		case 'stsai':
-			return {
-				id: '00004',
-				name: 'Roll Tsai',
-				nickname: 'Roll T.',
-				email: 'stsai@a10networks.com'
-			};
-		case 'shuang':
-			return {
-				id: '00005',
-				name: 'Shih-Ming Huang',
-				nickname: 'Shih-Ming H.',
-				email: 'shuang@a10networks.com'
-			};
-		case 'sho':
-			return {
-				id: '00006',
-				name: 'Shau-Hua Ho',
-				nickname: 'Shau-Hua H.',
-				email: 'sho@a10networks.com'
-			};
-		case 'kfong':
-			return {
-				id: '00007',
-				name: 'Kuang-Hui Fong',
-				nickname: 'Kuang-Hui F.',
-				email: 'kfong@a10networks.com'
-			};
-		case 'zli':
-			return {
-				id: '00008',
-				name: 'Zuoping Li',
-				nickname: 'Zuoping L.',
-				email: 'zli@a10networks.com'
-			};
-		case 'admin':
-			return {
-				id: '00000',
-				name: 'Admin',
-				nickname: 'Admin',
-				email: 'admin',
-				privilege: 10
-			};
-		default:
-			return {
-				id: '99999',
-				name: 'Tester',
-				nickname: 'Tester',
-				email: 'tester@a10networks.com'
-			};
-	}
+            return 'Update successfully!';
+        }
+    },
 };
 
 export const loginHandler = async (req, res) => {
-	let { account, password } = req.body;
+    let { account, password } = req.body;
 
-	// if (!(account.includes('@') || account.includes('\\'))) {
- //        account = LDAP_AUTH_PREFIX + account;
- //    }
-	try {
-        // let auth = await adPromise(account, password);
-        let user = fakeExtract(account);
+    try {
+        let loginInfo = await loginPromise(account, password);
+        let privilege = 0;
+        switch(loginInfo['sAMAccountName']) {
+            case 'zli':
+            case 'stsai':
+                privilege = 10;
+                break;
+            default:
+                privilege = 5;
+        }
+        let user = {
+            id: loginInfo['uSNCreated'],
+            name: loginInfo['displayName'],
+            nickname: loginInfo['givenName'],
+            email: loginInfo['mail'],
+            title: loginInfo['title'],
+            privilege: privilege
+        };
         let token = jwt.sign(user, SECURE_KEY, {
             expiresIn: '30 days'
         });
         let connection = null,
-			result = null,
-			query = null;
-		connection = await r.connect({ host: DB_HOST, port: DB_PORT });
-		query = r.db('work_genius').table('users').get(user.id);
-		result = await query.run(connection);
-		if (!result) {
-			query = r.db('work_genius').table('users').insert(user);
-		} else {
-			query = r.db('work_genius').table('users').get(user.id).update(user);
-		}
-		result = await query.run(connection);
-		await connection.close();
+            result = null,
+            query = null;
+        connection = await r.connect({ host: DB_HOST, port: DB_PORT });
+        query = r.db('work_genius').table('users').get(user.id);
+        result = await query.run(connection);
+        if (!result) {
+            query = r.db('work_genius').table('users').insert(user);
+        } else {
+            query = r.db('work_genius').table('users').get(user.id).update(user);
+        }
+        result = await query.run(connection);
+        await connection.close();
         res.json({
             success: true,
             token: token,
