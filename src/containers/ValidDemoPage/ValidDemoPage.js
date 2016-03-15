331256d086d3de0295342d8adf51ee493d1330bf
aqutw <aquarian.ex+github@gmail.com>
2016-03-15 19:16:27 +0800

    Add /main/valid-demo/ page

---
+++ src/containers/ValidDemoPage/ValidDemoPage.js
@@ -0,0 +1,15 @@
+// Libraries
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ValidDemoPage extends Component {
+  render() {
+    return (<section>
+      <h1>Valid Demo Page</h1>
+      {'xxxx'}
+    </section>
+    );
+  }
+}
+
+export default connect()(ValidDemoPage);
