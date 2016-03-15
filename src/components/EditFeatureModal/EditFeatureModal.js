46e2c4f559c6ea573014fa30e967c9d8e9ec0343
aqutw <aquarian.ex+github@gmail.com>
2016-03-15 15:18:12 +0800

    [TaskPage] re-style TaskPage with material-ui

--- src/components/EditFeatureModal/EditFeatureModal.js
+++ src/components/EditFeatureModal/EditFeatureModal.js
@@ -2,15 +2,19 @@
 import './EditFeatureModal.css';
 // Libraries
 import React, { Component, PropTypes } from 'react';
-import classnames from 'classnames';
+// import classnames from 'classnames';
 import moment from 'moment';
 // Components
 import {
-	Button,
-	Modal,
-	Input
+	Modal
 } from 'react-bootstrap';
-import DateTimeField from 'react-bootstrap-datetimepicker';
+// import DateTimeField from 'react-bootstrap-datetimepicker';
+
+import util from '../../libraries/util';
+import RaisedButton from 'material-ui/lib/raised-button';
+import TextField from 'material-ui/lib/text-field';
+import DatePicker from '../../components/A10-UI/Input/Date-Picker';
+import DropDownList from '../../components/A10-UI/Input/Drop-Down-List';
 
 let ModalHeader = ({ data }) => {
 	if (data.id) {
@@ -29,17 +33,17 @@ let ModalHeader = ({ data }) => {
 
 let ModalFooter = ({ data, onSubmit, onCancelHandler }) => {
 	let confirmButtonHtml = (
-		<Button onClick={onSubmit}>Create</Button>
+		<RaisedButton label="Create" secondary={true} onClick={onSubmit} />
 	);
 	if (data.id) {
 		confirmButtonHtml = (
-			<Button onClick={onSubmit}>Edit</Button>
+			<RaisedButton label="Edit" secondary={true} onClick={onSubmit} />
 		);
 	}
 	return (
 		<Modal.Footer>
             {confirmButtonHtml}
-            <Button onClick={onCancelHandler}>Cancel</Button>
+            <RaisedButton label="Cancel" onClick={onCancelHandler} />
         </Modal.Footer>
 	);
 };
@@ -57,7 +61,7 @@ class EditFeatureModal extends Component {
 	}
 
 	_validateInput() {
-		let devPercent = this.refs.dev_percent.getValue();
+		let devPercent = this.refs.dev_percent.value;
 		let eta = this.refs.eta.value;
 		if (devPercent < 0 || devPercent > 100 || devPercent.includes('e') || !devPercent) {
 			this.setState({
@@ -80,13 +84,13 @@ class EditFeatureModal extends Component {
 
 	_onSubmit() {
 		const { onSubmitHandler, formOptions } = this.props;
-		let title = this.refs.title.getValue();
-		let dev_id = this.refs.dev_id.getValue();
-		let project = this.refs.project.getValue();
-		let pri = this.refs.pri.getValue();
-		let dev_percent = this.refs.dev_percent.getValue() + '%';
+    let title = this.refs.title.value;
+		let dev_id = this.refs.dev_id.value;
+		let project = this.refs.project.value;
+		let pri = this.refs.pri.value;
+		let dev_percent = this.refs.dev_percent.value + '%';
 		let eta = this.refs.eta.value;
-		let owner_name = this.refs.owner_name.getValue();
+		let owner_name = this.refs.owner_name.value;
 		let dev_name = formOptions.devs.filter((dev) => dev.id === dev_id).map((user) => user.name)[0];
 		let data = {
 			title,
@@ -107,12 +111,15 @@ class EditFeatureModal extends Component {
 	}
 	_onDateChange(newDate) {
 		this.refs.eta.value = newDate;
-		this._validateInput();
 	}
-
+  _isValidPercent(val) {
+    const MIN_PERCENT = 0;
+    const MAX_PERCENT = 100;
+    return util.isValidNumber(val, {min:MIN_PERCENT, max:MAX_PERCENT}); 
+  }
 	render() {
-		const { show, data, onHideHandler, formOptions } = this.props;
-		let developerOptionsHtml = formOptions['devs'].map((developer, i) => {
+    const { show, data, onHideHandler, formOptions } = this.props;
+		/*let developerOptionsHtml = formOptions['devs'].map((developer, i) => {
 			return (
 				<option value={developer.id} key={i}>{developer.name}</option>
 			);
@@ -135,20 +142,40 @@ class EditFeatureModal extends Component {
 		let etaClassName = classnames({
 			'form-group': true,
 			'has-error': this.state.isEtaInvalid
-		});
+		});*/
+
+		let today = moment().format('YYYY-MM-DD');
+    let etaValue = data.eta || today;
+
+    if (data && data.dev_percent) {
+      data.dev_percent = data.dev_percent 
+        && data.dev_percent.replace 
+        && data.dev_percent.replace('%', '');  //<-- for <TextField type="number" ... /> of material-ui
+    }
+
 		return (
 			<Modal show={show} onHide={onHideHandler}>
 				<ModalHeader {...this.props} />
 				<Modal.Body>
 					<form className="form-horizontal">
+              {/*
 					    <Input
 					        type="text"
 					        label="Title"
 					        labelClassName="col-xs-2"
 					        wrapperClassName="col-xs-10"
 					        defaultValue={data.title ? data.title : ''}
-					        ref="title"/>
-					    <Input
+					        ref="title"/>*/}
+					    <div>
+							  <label className="col-xs-2 control-label">Title</label>
+                <input type="hidden" ref="title" defaultValue={data.title || ''} />
+					    	<div className="col-xs-9">
+					    		<TextField onChange={(evt)=>{
+                    this.refs.title.value = evt.target.value;
+                  }} defaultValue={data.title || ''} />
+					    	</div>
+						  </div>
+					    {/*<Input
 					        type="select"
 					        label="Developer"
 					        labelClassName="col-xs-2"
@@ -156,8 +183,22 @@ class EditFeatureModal extends Component {
 					        defaultValue={data.dev_id ? data.dev_id : ''}
 					        ref="dev_id">
 					        {developerOptionsHtml}
-					    </Input>
-					    <Input
+					    </Input>*/}
+					    <div>
+					    	<input type="hidden" ref="dev_id" defaultValue={data.dev_id || ''} />
+							  <label className="col-xs-2 control-label">Developer</label>
+					    	<div className="col-xs-9">
+					    		<DropDownList title={data.dev_id || ''}
+					    		  isDropDownListVisual2={true}
+				            onOptionClick={(val)=>{
+		                	this.refs.dev_id.value = val;
+		                }}
+				            aryOptionConfig={formOptions['devs'].map((item) => {
+		                  return {title: item.name, value: item.id, subtitle: ''};
+		                })} />
+					    	</div>
+						  </div>
+					    {/*<Input
 					        type="select"
 					        label="Project"
 					        labelClassName="col-xs-2"
@@ -165,8 +206,22 @@ class EditFeatureModal extends Component {
 					        defaultValue={data.project ? data.project : ''}
 					        ref="project">
 					        {projectOptionsHtml}
-					    </Input>
-					    <Input
+					    </Input>*/}
+					    <div>
+					      <input type="hidden" ref="project" defaultValue={data.project || ''} />
+							  <label className="col-xs-2 control-label">Project</label>
+					    	<div className="col-xs-9">
+					    	  <DropDownList title={data.project || ''}
+					    		  isDropDownListVisual2={true}
+		                onOptionClick={(val)=>{
+		                	this.refs.project.value = val;
+		                }}
+		                aryOptionConfig={formOptions['project'].map((item) => {
+		                	return {title: item, value: item, subtitle: ''};
+		                })} />
+					    	</div>
+						  </div>
+					    {/*<Input
 					        type="select"
 					        label="Priority"
 					        labelClassName="col-xs-2"
@@ -174,8 +229,24 @@ class EditFeatureModal extends Component {
 					        defaultValue={data.pri ? data.pri : ''}
 					        ref="pri">
 					        {priorityOptionsHtml}
-					    </Input>
-					    <Input
+					    </Input>*/}
+					    <div>
+					      <input type="hidden" ref="pri" defaultValue={data.pri || ''} />
+							  <label className="col-xs-2 control-label">Priority</label>
+					    	<div className="col-xs-9">
+					    		<DropDownList title={data.pri || ''}
+					    	    isDropDownListVisual2={true}
+		                onOptionClick={(val)=>{
+		                	this.refs.pri.value = val;
+		                }}
+		                aryOptionConfig={formOptions['pri'].filter((val)=>{ //filter and remove empty value
+		                	return !!val;
+		                }).map((item) => {
+		                	return {title: item, value: item, subtitle: ''};
+		                })} />
+					      </div>
+						  </div>
+					    {/*<Input
 					        type="number"
 					        min={0}
 					        max={100}
@@ -185,25 +256,50 @@ class EditFeatureModal extends Component {
 					        defaultValue={data.dev_percent ? parseInt(data.dev_percent, 10) : 0}
 					        bsStyle={this.state.isDevPercentInvalid ? 'error' : undefined}
 					        onChange={this._validateInput}
-					        ref="dev_percent" />
+					        ref="dev_percent" />*/}
+					    <div>
+                <input type="hidden" ref="dev_percent" defaultValue={
+                  this._isValidPercent(data.dev_percent) ? data.dev_percent : ''
+                } />
+							  <label className="col-xs-2 control-label">Progress</label>
+					      <div className="col-xs-9">
+					        <TextField hintText="0 ~ 100" type="number" defaultValue={
+                    this._isValidPercent(data.dev_percent) ? data.dev_percent : ''
+                  } onChange={(evt)=>{
+                    this.refs.dev_percent.value = evt.target.value;
+                  }} />{'%'}
+					      </div>
+						  </div>
+
 					    <input
 					        className="hidden"
-					        defaultValue={data.eta || moment().format('YYYY-MM-DD')}
+					        defaultValue={etaValue}
 					        onChange={this._validateInput}
 					        ref="eta" />
+              {/*
 					    <div className={etaClassName}>
 						    <label className="col-xs-2 control-label">ETA</label>
 						    <div className="col-xs-10">
 						        <DateTimeField
-						            dateTime={data.eta || moment().format('YYYY-MM-DD')}
+						            dateTime={etaValue}
 						            format="YYYY-MM-DD"
 						            inputFormat="YYYY-MM-DD"
 						            mode="date"
 						            showToday
 						            onChange={this._onDateChange} />
 						    </div>
-						</div>
-					    <Input
+						  </div>
+              */}
+						  <div>
+						    <label className="col-xs-2 control-label">ETA</label>
+						    <div className="col-xs-9">
+						        <DatePicker defaultDate={etaValue} placeholder="ETA" onChange={(val)=>{
+						    		this._onDateChange(val);
+						    	}} />
+						    </div>
+						  </div>
+
+					    {/*<Input
 					        type="select"
 					        label="Assignee"
 					        labelClassName="col-xs-2"
@@ -211,11 +307,26 @@ class EditFeatureModal extends Component {
 					        defaultValue={data.owner_name ? data.owner_name : ''}
 					        ref="owner_name">
 					        {assigneeOptionsHtml}
-					    </Input>
-					</form>
+					    </Input>*/}
+					    <div>
+					      <input type="hidden" ref="owner_name" defaultValue={data.owner_name || ''} />
+							  <label className="col-xs-2 control-label">Assignee</label>
+					    	<div className="col-xs-9">
+					    		<DropDownList title={data.owner_name || ''}
+					    		isDropDownListVisual2={true}
+				                onOptionClick={(val)=>{
+				                	this.refs.owner_name.value = val;
+				                }}
+				                aryOptionConfig={formOptions['owner_name'].map((item) => {
+				                	return {title: item, value: item, subtitle: ''};
+				                })} />
+					    	</div>
+						  </div>
+						<div style={{clear:'both'}}></div>
+					  </form>
 		        </Modal.Body>
 		        <ModalFooter
-		        	onSubmit={this._onSubmit}
+              onSubmit={this._onSubmit}
 		            {...this.props} />
 			</Modal>
 		);
