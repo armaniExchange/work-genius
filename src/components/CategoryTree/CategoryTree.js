bc90de2f4619d9fa3468f60c79a6317fdc2b9500
william` <lflower1206@gmail.com>
2016-03-21 16:46:18 +0800

    fix CategoryTree bug

--- src/components/CategoryTree/CategoryTree.js
+++ src/components/CategoryTree/CategoryTree.js
@@ -4,36 +4,55 @@ import React, { Component, PropTypes } from 'react';
 class CategoryTree extends Component {
 
   buildCategoryTree(categories, tree) {
-    let root = tree || {
-      'id': 'root',
-      'root': true,
-      'leaves': []
-    };
 
-    let legacyCategories = categories.filter(category => {
+    let filterCategories;
+
+    if (! tree) {
+
+      filterCategories = categories.filter(category => {
+
+        if (category['name'] === 'root') {
+          category['leaves'] = [];
+          tree = category;
+          return false;
+        }
+
+        return true;
+      });
+
+    } else {
+
+      filterCategories = categories;
+
+    }
+
+    let legacyCategories = filterCategories.filter(category => {
       
-      if (category.hasOwnProperty('parentId') && category['parentId']) {
-        let isFound = this.traversalCategoryTree(root, category);
+      if (category['parentId']) {
+        let isFound = this.traversalCategoryTree(tree, category);
         return ! isFound;
-      } else {
-        root['leaves'].push(category);
-        return false;
       }
 
     });
 
     if (legacyCategories && legacyCategories.length) {
-      this.buildCategoryTree(legacyCategories, root);
+      this.buildCategoryTree(legacyCategories, tree);
     }
 
-    return root;
+    return tree;
 
   }
 
   traversalCategoryTree(node, category) {
     let isFound = false;
 
-    if (node && node['leaves']) {
+    if (node['id'] === category['parentId']) {
+      category['leaves'] = [];
+      node['leaves'].push(category);
+      isFound = true;
+    }
+
+    if (! isFound && node['leaves'] && node['leaves'].length) {
 
       node['leaves'].some(leaf => {
         if (leaf['id'] === category['parentId']) {
@@ -46,25 +65,61 @@ class CategoryTree extends Component {
           return isFound;
         }
       });
+
     }
 
     return isFound;
   }
 
-  renderTree(node, level) {
-    let leaves = node['leaves'] || [];
-    level = level || 1;
+  renderTree(node={}, level = 0) {
 
-    return (
-      <ul key={node['id']}>
-        <li><a href="#">{node['name']}</a></li>
-        {
-          leaves.map(leaf => {
-            return this.renderTree(leaf, (level + 1));
-          })
-        }
-      </ul>
-    );
+    const styles = {
+      countStyle: {
+        float: 'right'
+      },
+      iconStyle: {
+        paddingRight: '0.5em'
+      },
+      nodeStyle: {
+        paddingTop: '4px',
+        paddingBottom: '4px'
+      }
+    };
+
+    let leaves = node['leaves'] || [];
+
+    let caretIcon = <i style={styles['iconStyle']}></i>;
+
+    if (leaves && leaves.length) {
+      caretIcon = <i style={styles['iconStyle']} className="fa fa-caret-down"></i>;
+    }
+
+    if (node['name'] === 'root') {
+      return (
+        <div>
+          {
+            leaves.map(leaf => {
+              return this.renderTree(leaf, level);
+            })
+          }
+        </div>
+      );
+    } else {
+      return (
+        <div style={{ paddingLeft: (level * 0.5) + 'em'}} key={node['id']}>
+          <div style={styles['nodeStyle']}>
+            {caretIcon}
+            <span>{node['name']}</span>
+            <span style={styles['countStyle']}>{node['articlesCount']}</span>
+          </div>
+          {
+            leaves.map(leaf => {
+              return this.renderTree(leaf, (level + 1));
+            })
+          }
+        </div>
+      );
+    }
   }
 
   render() {
