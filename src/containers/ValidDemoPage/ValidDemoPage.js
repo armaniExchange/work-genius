597ef6100b8b7723a431b03bc3e3b59fdc92fc06
aqutw <aquarian.ex+github@gmail.com>
2016-03-22 17:39:30 +0800

    [Valid] finished Input-Valid-Character

--- src/containers/ValidDemoPage/ValidDemoPage.js
+++ src/containers/ValidDemoPage/ValidDemoPage.js
@@ -4,30 +4,39 @@ import { connect } from 'react-redux';
 
 import { InputValidNumber, InputValidCharacter } from '../../components/A10-UI/Valid/';
 
+let DemoBox = ({title, children}) => {
+  return (<dl>
+      <dt>{title}</dt>
+      <dd style={{padding:'0 0 0 50px'}}>{children}</dd>
+    </dl>);
+};
+
 class ValidDemoPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
       pkvalue1:'kk', pkvalue2:'kk@', pkvalue3:'yy#yy', pkvalue4:'zz/zz',
-      character1:'kk', character2:'kk@', character3:'yyyy', character4:'zzzz',
+      character1:'kk', character2:'kk@', character3:'yyyy', character4:'9999',
       num1:'kk', num2:1, num3:2, num4:'10'
     };
   }
   render() {
     return (<section>
-      <h1>Valid Demo Page</h1>
-      <InputValidNumber defaultValue={this.state.num1} />
-      <InputValidNumber min={3} max={9} defaultValue={this.state.num2} />
-      <InputValidNumber min={3} defaultValue={this.state.num3} />
-      <InputValidNumber max={9} defaultValue={this.state.num4} />
-      <InputValidNumber max={9} defaultValue={this.state.num4} />
-      <hr />
-      <InputValidCharacter defaultValue={this.state.character1} />
-      <InputValidCharacter min={3} max={9} defaultValue={this.state.character2} />
-      <InputValidCharacter min={3} defaultValue={this.state.character3} />
-      <InputValidCharacter max={9} defaultValue={this.state.character4} />
-      <InputValidCharacter max={9} defaultValue={this.state.character4} />
-      <hr />
+      <h4>Valid Demo Page</h4>
+      <DemoBox title="InputValidNumber">
+        <InputValidNumber defaultValue={this.state.num1} />
+        <InputValidNumber min={3} max={9} defaultValue={this.state.num2} />
+        <InputValidNumber min={3} defaultValue={this.state.num3} />
+        <InputValidNumber max={9} defaultValue={this.state.num4} />
+        <InputValidNumber max={9} defaultValue={this.state.num4} />
+      </DemoBox>
+      <DemoBox title="InputValidCharacter">
+        <InputValidCharacter defaultValue={this.state.character1} />
+        <InputValidCharacter min={3} max={9} characterType="onlyLetterNumberSp" defaultValue={this.state.character2} />
+        <InputValidCharacter min={3} defaultValue={this.state.character3} />
+        <InputValidCharacter max={9} defaultValue={this.state.character4} />
+        <InputValidCharacter max={9} characterType="onlyLetterSp" defaultValue={this.state.character4} />
+      </DemoBox>
       {/*
       <InputValidPrimarykey defaultValue={this.state.pkvalue1} />
       <InputValidPrimarykey min={3} max={9} defaultValue={this.state.pkvalue2} />
