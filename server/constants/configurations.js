f24dbf05bc3004c9f21a89aefab3737ed675e7a0
Howard Chang <howard3433@gmail.com>
2016-03-29 09:20:42 +0800

    Modify crawling path

--- server/constants/configurations.js
+++ server/constants/configurations.js
@@ -23,8 +23,8 @@ export const ADMIN_ID = '00000';
 // THESE URLs WILL BE COMING FROM DB, This section is DEV ONLY
 export const GK2_BJ_4_1_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/227573/bugs/list/must-fix/?pb_status=open&team=10&group_by_member=yes&must_fix=on';
 export const GK2_SJ_4_1_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/227573/bugs/list/must-fix/?pb_status=open&team=33&group_by_member=yes&must_fix=on';
-export const GK2_BJ_3_2_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/283949/bugs/list/must-fix/?pb_status=open&team=10&group_by_member=yes&must_fix=on';
-export const GK2_SJ_3_2_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/283949/bugs/list/must-fix/?pb_status=open&team=33&group_by_member=yes&must_fix=on';
+export const GK2_BJ_3_2_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/658235/bugs/list/must-fix/?pb_status=open&team=10&group_by_member=yes&must_fix=on';
+export const GK2_SJ_3_2_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/658235/bugs/list/must-fix/?pb_status=open&team=33&group_by_member=yes&must_fix=on';
 export const GK2_BJ_4_0_3_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/518503/bugs/list/must-fix/?pb_status=open&team=10&group_by_member=yes&must_fix=on';
 export const GK2_SJ_4_0_3_MUST_FIX_BUG_URL = 'https://gk2.a10networks.com/projects/518503/bugs/list/must-fix/?pb_status=open&team=33&group_by_member=yes&must_fix=on';
 
@@ -39,8 +39,8 @@ export const GK2_MUST_FIX_BUG_URLS = [
 
 export const GK2_BJ_4_1_FEATURE_URL = 'https://gk2.a10networks.com/projects/227573/features/list/?search=&status=0&priority=0&owner=0&team=10&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
 export const GK2_SJ_4_1_FEATURE_URL = 'https://gk2.a10networks.com/projects/227573/features/list/?search=&status=0&priority=0&owner=0&team=33&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
-export const GK2_BJ_3_2_FEATURE_URL = 'https://gk2.a10networks.com/projects/283949/features/list/?search=&status=0&priority=0&owner=0&team=10&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
-export const GK2_SJ_3_2_FEATURE_URL = 'https://gk2.a10networks.com/projects/283949/features/list/?search=&status=0&priority=0&owner=0&team=33&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
+export const GK2_BJ_3_2_FEATURE_URL = 'https://gk2.a10networks.com/projects/658235/features/list/?search=&status=0&priority=0&owner=0&team=10&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
+export const GK2_SJ_3_2_FEATURE_URL = 'https://gk2.a10networks.com/projects/658235/features/list/?search=&status=0&priority=0&owner=0&team=33&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
 export const GK2_BJ_4_0_3_FEATURE_URL = 'https://gk2.a10networks.com/projects/518503/features/list/?search=&status=0&priority=0&owner=0&team=10&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
 export const GK2_SJ_4_0_3_FEATURE_URL = 'https://gk2.a10networks.com/projects/518503/features/list/?search=&status=0&priority=0&owner=0&team=33&category=149&task_type=0&task_status=0&task_priority=0&task_owner=0&document_status=yes&document_type=0';
 
