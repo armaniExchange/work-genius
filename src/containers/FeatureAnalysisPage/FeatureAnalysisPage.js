245e5430283935a7cc5df065ed13f68cd3636866
aqutw <aquarian.ex+github@gmail.com>
2016-03-31 10:07:35 +0800

    [assignment] currentLeaf WIP

--- src/containers/FeatureAnalysisPage/FeatureAnalysisPage.js
+++ src/containers/FeatureAnalysisPage/FeatureAnalysisPage.js
@@ -12,7 +12,9 @@ import RaisedButton from 'material-ui/lib/raised-button';
 class FeatureAnalysisPage extends Component {
     constructor(props){
       super(props);
-      this.user = [{title:'Zli', 'value': 'zli'}];
+      this.user = [{title:'Zuoping', 'value': 'zli'}, 
+                   {title:'Yuhua', 'value': 'yli'}
+                  ];
       this.level = [
                   {title:'Very Hard', 'value':9},
                   {title:'Hard', 'value':7},
@@ -31,26 +33,40 @@ class FeatureAnalysisPage extends Component {
     }
     render() {
         const {
+            currentLeaf,
             treeDataSource,
             updateOneAssignmentCategory,
             setCurrentLeafNode
         } = this.props;
+        console.log('currentLeaf, this.user', currentLeaf, this.user);
+        const displayForm = currentLeaf.id ? '' : 'none';
+        const displayHint = currentLeaf.id ? 'none' : '';
+        currentLeaf.primary_owner = 'yli';
+        currentLeaf.secondary_owner = 'zli';
+        currentLeaf.difficulty = 7;
         return (
             <div className="row">
-                <div className="pull-left col-md-6">
+                <div className="pull-left col-md-4">
+            {currentLeaf.primary_owner}
+            {currentLeaf.secondary_owner}
+            {currentLeaf.difficulty}
                     <AssignmentCategoryTree
                             data={treeDataSource}
                             onNodeClick={::this._onNodeClick}
                             onLeafClick={setCurrentLeafNode}/>
                 </div>
-                <div className="pull-right col-md-6">
-                  <div className="form-horizontal">
-                    <h3>Edit {this.props.curCategory}</h3>
+                <div className="pull-right col-md-8">
+                  <div style={{display:displayHint, fontSize:'30px', padding:'30px 0'}}>
+                    Please choose and click leaf in root tree.
+                  </div>
+                  <div className="form-horizontal" style={{display:displayForm}}>
+                    <h5 style={{color:'#999'}}>{'Edit '}<span style={{color:'#000'}}>{currentLeaf.path}</span></h5>
                     <div className="col-xs-3">
                       Primary Owner:
                     </div>
                     <div className="col-xs-9">
                       <DropDownList
+                        title={currentLeaf.primary_owner}
                         isDropDownListVisual2={true}
                         isNeedAll={false}
                         onOptionClick={(val) => {
@@ -63,6 +79,7 @@ class FeatureAnalysisPage extends Component {
                     </div>
                     <div className="col-xs-9">
                       <DropDownList
+                        title={currentLeaf.secondary_owner}
                         isDropDownListVisual2={true}
                         isNeedAll={false}
                         onOptionClick={(val) => {
@@ -71,10 +88,11 @@ class FeatureAnalysisPage extends Component {
                         aryOptionConfig={this.user} />
                     </div>
                     <div className="col-xs-3">
-                      Level:
+                      Difficulty:
                     </div>
                     <div className="col-xs-9">
                       <DropDownList
+                        title={currentLeaf.difficulty}
                         isDropDownListVisual2={true}
                         isNeedAll={false}
                         onOptionClick={(val) => {
@@ -99,13 +117,15 @@ class FeatureAnalysisPage extends Component {
 
 FeatureAnalysisPage.propTypes = {
     treeDataSource: PropTypes.object.isRequired,
-    curCategory: PropTypes.string,
+    currentLeaf: PropTypes.object.isRequired,
     updateOneAssignmentCategory: PropTypes.func.isRequired,
     fetchAssignmentCategories: PropTypes.func.isRequired,
     setCurrentLeafNode: PropTypes.func.isRequired,
     setFormVisibility: PropTypes.func.isRequired
 };
-FeatureAnalysisPage.defaultProps = {};
+FeatureAnalysisPage.defaultProps = {
+    currentLeaf: {}
+};
 
 function mapStateToProps(state) {
     return Object.assign(
