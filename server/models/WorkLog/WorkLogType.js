b9c04fdcb7f7029a14b3ac22dffba28cc0db2a98
mountain2004 <mountain2004@163.com>
2016-04-08 15:05:26 +0800

    add method to create/update worklog

---
+++ server/models/WorkLog/WorkLogType.js
@@ -0,0 +1,40 @@
+// GraphQL types
+import {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLID,
+    GraphQLInt
+} from 'graphql';
+
+const WorkLog_TYPE = new GraphQLObjectType({
+    name: 'worklog',
+    descriptyion: 'An bug object',
+    fields: () => ({
+        'id': {
+            type: GraphQLID,
+            description: 'work log ID'
+        },
+        'employee_id': {
+            type: GraphQLString,
+            description: 'employee ID'
+        },
+        'date': {
+        	type: GraphQLInt,
+        	description: 'date'
+        },
+        'content': {
+        	type: GraphQLString,
+        	description: 'work log content'
+        },
+        'progress': {
+        	type: GraphQLInt,
+        	description: 'progress'
+        },
+        'type': {
+            type: GraphQLString,
+            description: 'workday/pto/holiday'
+        }
+    })
+});
+
+export default WorkLog_TYPE;
