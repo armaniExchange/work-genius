90e15ba48122248b6d678ab1d47cbcba131b7c03
aqutw <aquarian.ex+github@gmail.com>
2016-04-08 18:58:03 +0800

    Add search-box at right side

--- src/components/Sub-Menu/Sub-Menu.js
+++ src/components/Sub-Menu/Sub-Menu.js
@@ -3,6 +3,7 @@ import './_Sub-Menu.css';
 
 // Libraries
 import React, { Component, PropTypes } from 'react';
+import TextField from 'material-ui/lib/text-field';
 import { Link } from 'react-router';
 
 class SubMenu extends Component {
@@ -24,6 +25,13 @@ class SubMenu extends Component {
 					<nav className="mdl-navigation">
 			            {linkHtml}
 					</nav>
+
+          <div className="search-box">
+            <TextField
+              hintText="Search..." />
+            <i className="material-icons" title="Search">search</i>
+          </div>
+
 			    </div>
 			</header>
 		);
