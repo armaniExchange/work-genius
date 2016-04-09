e05b961fe73a222776d5a0224458529f1f97f732
zuopingli <zli@a10networks.com>
2016-04-09 10:21:39 +0800

    migrate to production env

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ killall nohup
 killall node
 #/etc/init.d/rethinkdb restart
 nohup npm run serverDev > server.log 2>&1 &
-nohup npm run dev > client.log 2>&1 &
+#nohup npm run dev > client.log 2>&1 &
 service nginx restart
-echo 'show 8001 netstat'
-netstat -ano|grep 3000
+#echo 'show 8001 netstat'
+#netstat -ano|grep 3000
 echo 'show 3000 netstat'
 netstat -ano|grep 8001
