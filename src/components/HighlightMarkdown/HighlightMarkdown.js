5130843456b45597a4fad791a8ae6293d81674f0
Steven Fong <fongkuanghui@gmail.com>
2016-04-25 15:42:39 +0800

    put image style in _HighlightMarkdown css

--- src/components/HighlightMarkdown/HighlightMarkdown.js
+++ src/components/HighlightMarkdown/HighlightMarkdown.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import Markdown from 'react-markdown';
 import CodeBlock from '../../components/CodeBlock/CodeBlock';
 
+import './_HighlightMarkdown.css';
+
 export default class HighlightMarkdown extends Component {
   render() {
     return (
       <Markdown
         {...this.props}
+        className="highlight-markdown"
         renderers={Object.assign({}, Markdown.renderes, {
           CodeBlock
        })} />
