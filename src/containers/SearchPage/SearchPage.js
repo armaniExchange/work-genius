e302565790963c782a9db66ba180578762b53cbc
mountain2004 <mountain2004@163.com>
2016-04-29 14:17:17 +0800

    comment the unused namespace 'moment'

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -2,7 +2,7 @@
 import './_SearchPage.css';
 // React & Redux
 import React, { Component, PropTypes } from 'react';
-import moment from 'moment';
+//import moment from 'moment';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 // Actions
