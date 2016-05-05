568f1b6b3fc004a214f6a36aaead5bf3a1b86eb9
Steven Fong <fongkuanghui@gmail.com>
2016-05-05 14:30:13 +0800

    article toolbar

--- src/containers/ViewArticlePage/ViewArticlePage.js
+++ src/containers/ViewArticlePage/ViewArticlePage.js
@@ -2,18 +2,11 @@
 import './_ViewArticlePage.scss';
 // React & Redux
 import React, { Component, PropTypes } from 'react';
-import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Paper from 'material-ui/lib/paper';
-import { SERVER_EXPORT_URL } from '../../constants/config';
 import HighlightMarkdown from '../../components/HighlightMarkdown/HighlightMarkdown';
-import IconMenu from 'material-ui/lib/menus/icon-menu';
-import MenuItem from 'material-ui/lib/menus/menu-item';
-import IconButton from 'material-ui/lib/icon-button';
-import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
-import Divider from 'material-ui/lib/divider';
-
+import ArticleToolbar from '../../components/ArticleToolbar/ArticleToolbar';
 import ArticleTagList from '../../components/ArticleTagList/ArticleTagList';
 import CommentEditor from '../../components/CommentEditor/CommentEditor';
 import CommentListItem from '../../components/CommentListItem/CommentListItem';
@@ -141,25 +134,10 @@ class ViewArticlePage extends Component {
             files={files}
           />
           <ArticleTagList tags={tags} />
-          <div style={{position: 'absolute', top: 10, right: 10}}>
-            <IconMenu
-              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-              targetOrigin={{horizontal: 'right', vertical: 'top'}}
-            >
-              <Link to={`/main/knowledge/document/edit/${id}`}>
-                <MenuItem primaryText="Edit" leftIcon={<i className="fa fa-edit" style={{fontSize: 24}}/>} />
-              </Link>
-              <MenuItem primaryText="Delete" leftIcon={<i className="fa fa-trash" style={{fontSize: 24}}/>} onClick={::this.onArticleDelete}/>
-              <Divider />
-              <a href={`${SERVER_EXPORT_URL}/document/${id}?token=${localStorage.token}`} download>
-                <MenuItem primaryText="Article PDF" leftIcon={<i className="fa fa-download" style={{fontSize: 24}}/>}/>
-              </a>
-              <a href={`${SERVER_EXPORT_URL}/document/${id}?withComments=true&token=${localStorage.token}`} download>
-                <MenuItem primaryText="Article with Comments PDF" leftIcon={<i className="fa fa-download" style={{fontSize: 24}}/>}/>
-              </a>
-            </IconMenu>
-          </div>
+          <ArticleToolbar
+            id={id}
+            onDelete={::this.onArticleDelete}
+          />
         </Paper>
         <Paper className="body" zDepth={1}>
           <HighlightMarkdown source={content} />
