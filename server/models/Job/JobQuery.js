99210b86ab54dcad1b69d0fc6f9e1a1ceb929e9a
mountain2004 <mountain2004@163.com>
2016-05-05 14:52:20 +0800

    add some logs to debug

--- server/models/Job/JobQuery.js
+++ server/models/Job/JobQuery.js
@@ -41,11 +41,19 @@ let JobQuery = {
 				}
 				let endDate = startDate + (dateRange -1) * 1000 * 3600 * 24;
 				query = r.db('work_genius').table('users').filter(r.row('id').ne(ADMIN_ID).and(r.row('id').ne(TESTER_ID)))
-					.filter(r.row('groups').contains(GUI_GROUP))
+					.filter(function(user){
+						return user('groups').default([]).contains(GUI_GROUP);
+					})
 					.pluck('id','name','location','timezone').orderBy('location').coerceTo('array');
 				connection = await r.connect({ host: DB_HOST, port: DB_PORT });
 				//get all users
 				let users = await query.run(connection);
+				console.log('GUI_GROUP:');
+				console.log(GUI_GROUP);
+				console.log();
+				console.log('users:');
+				console.log(users);
+				console.log();
 
 
 				//get all PTO data
