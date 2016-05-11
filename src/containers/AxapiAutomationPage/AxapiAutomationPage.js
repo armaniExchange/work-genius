a5bc67d55271946b54f8bb5d6002bc01a1219f74
aqutw <aquarian.ex+github@gmail.com>
2016-05-11 17:05:45 +0800

    [axapi_auto] add schema_changes page

--- src/containers/AxapiAutomationPage/AxapiAutomationPage.js
+++ src/containers/AxapiAutomationPage/AxapiAutomationPage.js
@@ -1,14 +1,25 @@
 // Style
 import './_AxapiAutomationPage.css';
 // React & Redux
-// import React, { Component, PropTypes } from 'react';
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import TextField from 'material-ui/lib/text-field';
 import RaisedButton from 'material-ui/lib/raised-button';
 import { bindActionCreators } from 'redux';
 // Actions
 import * as AxapiAutomationPageActions from '../../actions/search-actions';
+
+let DiffLabelTag = (props) => {
+  let _style = {padding:'3px 16px', textAlign:'center', color:'#333', background:'#ccc', 'margin': '0 6px 0 0'};
+  if (props.emphasize) {
+    _style.color = '#fff';
+    _style.background = '#b00';
+  }
+  return <span style={_style}>{props.children}</span>;
+};
+DiffLabelTag.propTypes = {
+  emphasize: PropTypes.bool
+};
 
 
 class AxapiAutomationPage extends Component {
@@ -52,7 +63,25 @@ class AxapiAutomationPage extends Component {
         </div>
         <div className="automation-page-right__body">
           {[1,2,3].map(()=>{
-            return (<div className="automation-page-right__body-row">
+            return (<div className="automation-page-right__body-row--schema">
+            <div className="automation-page-right__body-row--schema__left" style={{float:'left','margin':'0 0 10px', width:'35%'}}>
+              <div>
+                <DiffLabelTag>CLI</DiffLabelTag>
+              </div>
+              <div style={{height:'260px', background:'#ddd'}}></div>
+            </div>
+            <div className="automation-page-right__body-row--schema__right" style={{float:'right','margin':'0 0 10px', width:'64%'}}>
+              <div>
+                <DiffLabelTag>Server</DiffLabelTag>
+                <DiffLabelTag>ServerGroup</DiffLabelTag>
+              </div>
+              <div style={{height:'260px', background:'#ddd'}}></div>
+            </div>
+          </div>);
+          })}
+
+          {[1,2,3].map(()=>{
+            return (<div className="automation-page-right__body-row--axapireq">
             <div className="automation-page-right__body-row__head">
               <span style={{float:'left','border-radius':'2px',background:'#b00',color:'#fff',padding:'2px 6px'}}>Fail</span>
               <span style={{float:'left','border-radius':'2px',background:'green',color:'#fff',padding:'2px 6px'}}>Success</span>
