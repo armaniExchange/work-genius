33db191dea5d6f85c9b931f2024355f84e244f3f
zuopingli <zli@a10networks.com>
2016-06-29 19:31:14 +0800

    auto upgrade on server almost done

---
+++ upgrade/devices_bj_demo.js
@@ -0,0 +1,47 @@
+module.exports = 
+[
+{
+"ip":  "192.168.105.86" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+},
+
+{
+"ip":  "192.168.105.196" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+},
+{
+"ip":  "192.168.105.99" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+},
+
+{
+"ip":  "192.168.105.197" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+},
+{
+"ip":  "192.168.105.72" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+},
+{
+"ip":  "192.168.105.82" ,
+"password":  "a10" ,
+"release":  "4_1_1" ,
+"user_name":  "admin",
+"with_fpga": false
+}
+];
