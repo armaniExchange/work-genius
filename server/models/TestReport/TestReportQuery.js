730ca47334da295aea9a732ac0c251feadcbd0a0
Steven Fong <fongkuanghui@gmail.com>
2016-07-05 10:34:14 +0800

    desc created time list

--- server/models/TestReport/TestReportQuery.js
+++ server/models/TestReport/TestReportQuery.js
@@ -21,7 +21,7 @@ async function getTestReportCreatedTimeList() {
   const connection = await r.connect({ host: DB_HOST, port: DB_PORT });
   const result = await r.db('work_genius').table('test_report_time_list')
     .group('type')
-    .orderBy('createdAt')
+    .orderBy(r.desc('createdAt'))
     .map(function(item){
       return item('createdAt');
     })
