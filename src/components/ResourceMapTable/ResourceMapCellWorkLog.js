de348b03a32e3ce4d3e4c91791ddbdaff4390e74
quesle <eneblue@126.com>
2016-07-12 14:53:57 +0800

    Modify tooltip in resource map

--- src/components/ResourceMapTable/ResourceMapCellWorkLog.js
+++ src/components/ResourceMapTable/ResourceMapCellWorkLog.js
@@ -4,49 +4,50 @@ import 'rc-tooltip/assets/bootstrap_white.css';
 import 'rc-checkbox/assets/index.css';
 
 import React, { Component, PropTypes } from 'react';
-import moment from 'moment';
+// import moment from 'moment';
 
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import Tooltip from 'rc-tooltip';
 import Checkbox from 'rc-checkbox';
+import ResourceDetailTooltipContent from './ResourceDetailTooltipContent';
 
 const TAG = 'bgm-teal';
 
-var TooltipContent = ({item}) => {
-	return (
-		<div className="mdl-grid" style={{ width: '300px'}}>
-			<div className="mdl-cell mdl-cell--6-col"><label>Today Percentage</label></div>
-			<div className="mdl-cell mdl-cell--6-col"><span>{item.daily_percentage}%</span></div>
+// var TooltipContent = ({item}) => {
+// 	return (
+// 		<div className="mdl-grid" style={{ width: '300px'}}>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Today Percentage</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col"><span>{item.daily_percentage}%</span></div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>Task</label></div>
-			<div className="mdl-cell mdl-cell--6-col"><span>{item.title}</span></div>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Task</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col"><span>{item.title}</span></div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>Start Date</label></div>
-			<div className="mdl-cell mdl-cell--6-col">
-				<span><em>{moment(item.start_date).format('YYYY-MM-DD HH:MM')}</em></span>
-			</div>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Start Date</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col">
+// 				<span><em>{moment(item.start_date).format('YYYY-MM-DD HH:MM')}</em></span>
+// 			</div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>End Date</label></div>
-			<div className="mdl-cell mdl-cell--6-col">
-				<span><em>{moment(item.end_date).format('YYYY-MM-DD HH:MM')}</em></span>
-			</div>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>End Date</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col">
+// 				<span><em>{moment(item.end_date).format('YYYY-MM-DD HH:MM')}</em></span>
+// 			</div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>Duration</label></div>
-			<div className="mdl-cell mdl-cell--6-col">
-				<span><em>{item.duration ? item.duration : 0}</em> Hours</span>
-			</div>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Duration</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col">
+// 				<span><em>{item.duration ? item.duration : 0}</em> Hours</span>
+// 			</div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>Work Log</label></div>
-			<div className="mdl-cell mdl-cell--6-col">
-				<span>{item.content}</span>
-			</div>
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Work Log</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col">
+// 				<span>{item.content}</span>
+// 			</div>
 
-			<div className="mdl-cell mdl-cell--6-col"><label>Progress</label></div>
-			<div className="mdl-cell mdl-cell--6-col"><em>{item.progress}%</em></div>
-		</div>
-	);
-};
+// 			<div className="mdl-cell mdl-cell--6-col"><label>Progress</label></div>
+// 			<div className="mdl-cell mdl-cell--6-col"><em>{item.progress}%</em></div>
+// 		</div>
+// 	);
+// };
 
 class ResourceMapCellWorkLog extends Component {
 
@@ -166,7 +167,7 @@ class ResourceMapCellWorkLog extends Component {
 								  	<Tooltip
 										placement="top"
 										overlay={
-											(<TooltipContent item={item} />)
+											(<ResourceDetailTooltipContent item={item} />)
 										}
 										mouseEnterDelay={1}
 										arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
