c072832491a71ebf9831dd9577414075bfb6448c
Steven Fong <fongkuanghui@gmail.com>
2016-11-24 17:52:49 +0800

    add Peter in PTO cc list

--- src/constants/pto-constants.js
+++ src/constants/pto-constants.js
@@ -4,4 +4,4 @@ export const DENIED = 'DENIED';
 export const CANCEL_REQUEST_PENDING = 'CANCEL REQUEST PENDING';
 export const CANCEL_REQUEST_APPROVED = 'CANCEL REQUEST APPROVED';
 
-export const HR_MANAGERS_EMAIL = ['JZeng@a10networks.com', 'WTung@a10networks.com'];
+export const HR_MANAGERS_EMAIL = ['JZeng@a10networks.com', 'WTung@a10networks.com', 'PChuang@a10networks.com'];
